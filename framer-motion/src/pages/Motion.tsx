1501b3b5941df6c3b2d27fb2f7b7ae5ff87b2479
Cupcakes33 <rute930627@gmail.com>
2023-04-02 20:17:22 +0900

    :sparkles: feat: drag

--- framer-motion/src/pages/Motion.tsx
+++ framer-motion/src/pages/Motion.tsx
@@ -1,29 +1,43 @@
-import styled from "styled-components";
+import { useRef } from "react";
+import styled, { css } from "styled-components";
 import { motion } from "framer-motion";
 
 const Motion = () => {
-  const framerOption = {
-    initial: { scale: 0 },
-    animate: {
-      scale: 1,
-      rotateZ: 360,
-      transition: { type: "spring", delay: 1, bounce: 0.5 },
-    },
+  const biggerBoxRef = useRef<HTMLDivElement>(null);
+  const boxMotion = {
+    hover: { scale: 1.5, rotateZ: 90 },
+    click: { borderRadius: "100px" },
+    drag: { backgroundColor: "#9a9a9a" },
   };
   return (
     <>
       <StyledMotion.Wrapper>
-        <StyledMotion.Box
-          variants={framerOption}
-          initial="initial"
-          animate="animate"
-        />
+        <StyledMotion.BigBox ref={biggerBoxRef}>
+          <StyledMotion.Box
+            variants={boxMotion}
+            drag
+            dragSnapToOrigin
+            dragElastic={0.3}
+            dragConstraints={biggerBoxRef}
+            whileHover="hover"
+            whileDrag="drag"
+            whileTap="click"
+          />
+        </StyledMotion.BigBox>
       </StyledMotion.Wrapper>
     </>
   );
 };
 
 export default Motion;
+
+const StyledOption = {
+  fc: css`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  `,
+};
 
 const StyledMotion = {
   Wrapper: styled.div`
@@ -33,12 +47,20 @@ const StyledMotion = {
     justify-content: center;
     align-items: center;
   `,
+  BigBox: styled.div`
+    width: 600px;
+    height: 600px;
+    background-color: rgba(255, 255, 255, 0.2);
+    border-radius: 40px;
+    overflow: hidden;
+    ${StyledOption.fc}
+  `,
 
   Box: styled(motion.div)`
     width: 200px;
     height: 200px;
     background-color: white;
     border-radius: 10px;
-    box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba;
+    box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
   `,
 };
